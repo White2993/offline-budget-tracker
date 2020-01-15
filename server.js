d6b6b9863fce26267659937409dc9d8e61dfea16
Zack White <zrwhite2993@gmail.com>
2020-01-15 10:09:23 -0600

    Fiddled with the port to maybe fix the Heroku issue

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
-const PORT = process.env.PORT || 3000;
-
 const app = express();
+
+app.set( 'port', ( process.env.PORT || 5000 ));
 
 app.use(logger("dev"));
 
@@ -23,6 +22,6 @@ mongoose.connect("mongodb://localhost/budget", {
 require('./routes/htmlRoutes.js')
 require('./routes/apiRoutes.js')(app)
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
